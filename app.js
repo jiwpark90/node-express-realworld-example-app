11593e70a530051e47380e336400773a23227324
Ji Park <jiwpark90@gmail.com>
2018-05-16 23:14:15 -0700

    no cors

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ var fs = require('fs'),
 
 var isProduction = process.env.NODE_ENV === 'production';
 
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
-  credentials: true
-}
+// var corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200,
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+//   credentials: true
+// }
 
 // Create global app object
 var app = express();
-app.options('*', cors(corsOptions));
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   next();
-// });
+// app.options('*', cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 // app.use(cors());
 
 // Normal express config defaults
