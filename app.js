04e05c1edcf6eb07240766199c5ba449c77176a6
Ji Park <jiwpark90@gmail.com>
2018-05-20 16:11:57 -0700

    wtf

--- app.js
+++ app.js
@@ -34,78 +34,78 @@ var corsOptions = {
 //   // else {
 //   //   next();
 //   // }
-// };
+// // };
 
-// Create global app object
-var app = express();
-app.use(function(req, res, next) {
-  return res.status(404);
-});
-// app.use(cors());
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
+// // Create global app object
+// var app = express();
+// app.use(function(req, res, next) {
+//   return res.status(404);
+// });
+// // app.use(cors());
+// // app.use(cors(corsOptions));
+// // app.options('*', cors(corsOptions));
 
-// Normal express config defaults
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// // Normal express config defaults
+// app.use(require('morgan')('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+// app.use(require('method-override')());
+// app.use(express.static(__dirname + '/public'));
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+// app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
+// if (!isProduction) {
+//   app.use(errorhandler());
+// }
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
-}
+// if(isProduction){
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect('mongodb://localhost/conduit');
+//   mongoose.set('debug', true);
+// }
 
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
-// TODO special syntax for registering routes?
-app.use(require('./routes'));
+// require('./models/User');
+// require('./models/Article');
+// require('./models/Comment');
+// require('./config/passport');
+// // TODO special syntax for registering routes?
+// app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// /// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-/// error handlers
+// /// error handlers
 
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
+// // development error handler
+// // will print stacktrace
+// if (!isProduction) {
+//   app.use(function(err, req, res, next) {
+//     console.log(err.stack);
 
-    res.status(err.status || 500);
+//     res.status(err.status || 500);
 
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
-}
+//     res.json({'errors': {
+//       message: err.message,
+//       error: err
+//     }});
+//   });
+// }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({'errors': {
+//     message: err.message,
+//     error: {}
+//   }});
+// });
 
 // finally, let's start our server...
 var server = app.listen( process.env.PORT || 3000, function(){
