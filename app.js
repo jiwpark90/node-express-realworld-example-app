f5441445eb918908044ae43149800a6feaf702f2
Ji Park <jiwpark90@gmail.com>
2018-05-20 16:03:55 -0700

    tewak

--- app.js
+++ app.js
@@ -21,17 +21,22 @@ var corsOptions = {
 }
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // // intercept OPTIONS method
+  // if ('OPTIONS' == req.method) {
+  //   res.send(200);
+  // }
+  // else {
+  //   next();
+  // }
 };
 
 // Create global app object
