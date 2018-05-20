e42261bcc84398e192ca13c930a13f472eaea742
Ji Park <jiwpark90@gmail.com>
2018-05-20 15:58:11 -0700

    tweak

--- app.js
+++ app.js
@@ -20,9 +20,24 @@ var corsOptions = {
   enablePreflight: true
 }
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 // Create global app object
 var app = express();
-app.use(cors());
+app.use(allowCrossDomain);
+// app.use(cors());
 // app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
 
